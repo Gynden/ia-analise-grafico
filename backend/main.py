98c4bf38f6a1d776af70a0c383502936a66fe445
Gynden <pedrolucasramos6@gmail.com>
2025-11-17 14:24:03 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["*"],   # libera geral
     allow_methods=["*"],
     allow_headers=["*"],
 )
